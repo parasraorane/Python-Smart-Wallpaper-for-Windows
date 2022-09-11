1aa9dec991e62d76a77002721ff9c03d5f51dc73
Paras Raorane <parasganeshraorane@gmail.com>
2022-09-11 19:22:39 +0530

    display time at the bottom right corner

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A python script that sets a new wallpaper adding following features to it:
 * Wallpaper Image is selected randomly from the specified folder
 * Random quote at the left bottom portion of wallpaper
 * Required Stock tickers with current price and percentage change at the top right corner of wallpaper  
+* Last updated time in the bottom right corner of wallpaper
 
 Schedule the execution of the script(.py file) using Task Scheduler so the script runs in the background at the required interval. (ref: https://www.youtube.com/watch?v=n2Cr_YRQk7o)
 
--- changewallpaper.py
+++ changewallpaper.py
@@ -1,20 +1,27 @@
 import requests,ctypes,os,numpy as np
 from PIL import Image,ImageFont,ImageDraw
 import yfinance as yf
+from datetime import datetime
 
-#adding tickers
-#list of tuples with first element as the ticker name from yahoo finance and second as the name to display in the wallpaper
-tickers=[("^GSPC","s&p500"),
-         ("aapl","aapl"),
-         ("tsla","tsla"),
-         ("^nsei","nf50"),
-         ("^nsebank","nfbnk"),
-         ("reliance.ns","ris"),
-        ("tcs.ns","tcs"),
-        ("infy.ns","infy"),
-        ("BTC-USD","btc")]
+#enable or disable any functionality
+enable_time=True
+enable_tickers=True
+enable_quote=True
 
-#function to pad wallpaper in correct resolution
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+tickers=[
+        ("^GSPC","s&p500"),
+        ("aapl","aapl"),
+        ("msft","msft"),
+        ("goog","googl"),
+        ("amzn","amzn"),
+        ("tsla","tsla"),
+        ("BTC-USD","btc"),
+        ("ETH-USD","eth")
+        ]
+
 def add_margin(img,top,left,bottom,right,color):
     width, height = img.size
     new_width = width + right + left
@@ -22,7 +29,6 @@ def add_margin(img,top,left,bottom,right,color):
     result = Image.new(img.mode, (new_width, new_height),color)
     result.paste(img, (left, top))
     return result
-#function to convert image to correct size
 def conv_to_size(path,path_to_save=None):
     z=Image.open(path)
     if z.size[0]/1920>z.size[1]/1080:
@@ -39,7 +45,6 @@ def conv_to_size(path,path_to_save=None):
         res.save(path_to_save)
     else:
         res.save(path)
-#function that takes ticker and returns 24hr change and current price rounded to 2 decimals
 def ret_change(ticker):
     x= yf.Ticker(ticker)
     z=x.history(period="3d")
@@ -51,37 +56,51 @@ z=os.listdir("path/to/folder/containing/images")
 path="path/to/folder/containing/images"+z[np.random.randint(len(z))]
 z=Image.open(path)
 
-#ensure the ratio of dimensions for wallpaper is correct
+#check if in correct proportion, pad if it isnt
 if z.size[0]/z.size[1]>1.78 or z.size[0]/z.size[1]<1.76:
     conv_to_size(path)
     z=Image.open(path)
+    
+#resizing to perfect resolution
 z=z.resize((1920,1080))
 
-try:#write a quote on the image if it is fetched succesfully
-    I1 = ImageDraw.Draw(z)
-    myFont = ImageFont.truetype("path/to/.ttf/font/file",20)
-    l="http://api.quotable.io/random"
-    r=requests.get(l)
-    r=r.json()
-    text=r["content"]+" - "+r["author"]
 
-    I1.text((10,1050), text, font=myFont, fill =(255, 255, 255))
+#start drawing
+I1 = ImageDraw.Draw(z)
+if enable_quote:     
+    try:#write a quote on the image if it is fetched succesfully
+        
+        myFont = ImageFont.truetype("path/to/.ttf/font/file",20)
+        l="http://api.quotable.io/random"
+        r=requests.get(l)
+        r=r.json()
+        text=r["content"]+" - "+r["author"]
     
-    #displaying tickers
-    ticpos=10
-    for i,j in tickers:
-        chn,pc=ret_change(i)
-        I1.text((1670,ticpos),j, font=myFont, fill =(255,255,255))
-        if chn>=0:
-            I1.text((1750,ticpos),str(chn)+"%", font=myFont, fill =(0,255,0))
-            I1.text((1830,ticpos),str(pc), font=myFont, fill =(0,255,0))
-        else:
-            I1.text((1750,ticpos),str(abs(chn))+"%", font=myFont, fill =(255,0,0))
-            I1.text((1830,ticpos),str(pc), font=myFont, fill =(255,0,0))
-        ticpos+=25
-except:
-    True
-    
+        I1.text((10,1050), text, font=myFont, fill =(255, 255, 255))
+    except:
+        pass
+if enable_time:
+    try:
+        I1.text((1830,1050), current_time, font=myFont, fill =(255, 255, 255))
+    except:
+        pass
+if enable_tickers:
+    try:
+        ticpos=10
+        for i,j in tickers:
+            chn,pc=ret_change(i)
+            I1.text((1670,ticpos),j, font=myFont, fill =(255,255,255))
+            if chn>=0:
+                I1.text((1750,ticpos),str(chn)+"%", font=myFont, fill =(0,255,0))
+                I1.text((1830,ticpos),str(pc), font=myFont, fill =(0,255,0))
+            else:
+                I1.text((1750,ticpos),str(abs(chn))+"%", font=myFont, fill =(255,0,0))
+                I1.text((1830,ticpos),str(pc), font=myFont, fill =(255,0,0))
+                
+            ticpos+=25
+    except:
+        pass
+
 try:#save as jpg
     save_path="path/to/save/temp/wallpaper/in/.jpg/format"
     z.save(save_path,quality="high")
